7f665bb595728c4c0c02625f0a1185f9098497d6
rcmonitor <rcmonitor@hotmail.com>
2020-06-12 18:54:29 +0300

    AD8495 for chamber heater

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -410,7 +410,7 @@
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_5 0
 #define TEMP_SENSOR_BED 1
-#define TEMP_SENSOR_CHAMBER 20
+#define TEMP_SENSOR_CHAMBER -4
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
 #define DUMMY_THERMISTOR_998_VALUE 25
